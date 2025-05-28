6bcf7668fb2880be8dca6857ce25b95b8e782c87
JuliusM5 <54817220+JuliusM5@users.noreply.github.com>
2025-05-28 14:16:25 +0300

    security

--- saukstas-meiles/src/firebase.js
+++ saukstas-meiles/src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm8IvXMfmbCDzgmf8-HEBhaCQ0dfp4ZAQ",
-  authDomain: "saukstasmeiles.firebaseapp.com",
-  projectId: "saukstasmeiles",
-  storageBucket: "saukstasmeiles.firebasestorage.app",
-  messagingSenderId: "58938164125",
-  appId: "1:58938164125:web:38183fad0e576db49a2574",
-  measurementId: "G-L7GH1YNQ10"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
